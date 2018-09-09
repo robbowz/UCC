2408f25044a2fb4cc1e69e3682ee18460dbf781c
Robert <36878317+robbowz@users.noreply.github.com>
2018-09-09 21:56:42 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+sudo apt-get -y install p7zip-full p7zip-rar
+
 function remove_old_files() {
 	echo "Removing old files..."
 	sudo killall uccd
@@ -16,7 +18,7 @@ function download_wallet() {
     cd ucc
 	mkdir /root/.ucccore
 	wget https://github.com/UCCNetwork/ucc/releases/download/v.2.0.0.0/UCC-Linux64-v.2.0.0.0.zip
-	tar -xvf UCC-Linux64-v.2.0.0.0.zip
+	7z +x UCC-Linux64-v.2.0.0.0.zip
 	rm /root/ucc/UCC-Linux64-v.2.0.0.0.zip
 	cp sovcore-1.3.1/bin/uccd /root/ucc/uccd
 	cp sovcore-1.3.1/bin/ucc-cli /root/ucc/ucc-cli
