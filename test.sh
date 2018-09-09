2850c9950fe71d3e86bc262b97ed92ad8a3bf0b2
Robert <36878317+robbowz@users.noreply.github.com>
2018-09-09 22:30:44 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ sudo apt-get -y install p7zip-full p7zip-rar
     cd ucc
 	mkdir /root/.ucccore
 	wget https://github.com/UCCNetwork/ucc/releases/download/v.2.0.0.0/UCC-Linux64-v.2.0.0.0.zip
-	7z +x UCC-Linux64-v.2.0.0.0.zip
+	7z e UCC-Linux64-v.2.0.0.0.zip
 	rm /root/ucc/UCC-Linux64-v.2.0.0.0.zip
 	cp sovcore-1.3.1/bin/uccd /root/ucc/uccd
 	cp sovcore-1.3.1/bin/ucc-cli /root/ucc/ucc-cli
